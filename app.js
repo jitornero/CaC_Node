c92e01b6774e66decfc753f661d574b77623bb80
jitornero <tornerojuan@gmail.com>
2022-06-10 03:29:04 -0300

    ejs instalado

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const res = require('express/lib/response');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hola Express!!')
+    res.render('index')
 });
 
 router.get('/productos/:codigo', (req,res) => {
